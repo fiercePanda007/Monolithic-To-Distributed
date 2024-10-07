8de2e556f71879c290ed305fe6330e104400619e
Hasib <h.hossain7864@gmail.com>
2024-10-07 10:33:53 +0600

    front end added

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from "express";
-
+import { fileURLToPath } from "url";
+import path from "path";
 import route from "./routes/rootRouter.js";
 import logger from "./middleWare/logger.js";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public"))); //Middleware: Works between the incoming request and outgoing response
 
 // Middleware to parse request body as JSON
 app.use(express.json());
